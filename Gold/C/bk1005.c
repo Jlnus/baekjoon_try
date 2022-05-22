4a496bcfacc9d25c573961d83456c0ed5defde31
Jlnus <zmdk0902@naver.com>
2022-05-23 05:53:41 +0900

    1005 in progress

--- Gold/C/bk1005.c
+++ Gold/C/bk1005.c
@@ -1,39 +1,59 @@
 #include <stdio.h>
 
-int get_time(int **rule, int *t, int W, int K, int res)
-{	
-	int sign = 0;
-	res += t[W];
-	for(int i = 0; i<K;i++){
-		if(rule[i][1] == W)
+/*
+int maxtime(int **total)
+
+{
+}
+*/
+
+// segmant 오류, 하나씩 돌려볼 것, 예제 마지막 이용
+void get_time(int (*rule)[2], int W, int (*total)[1000], int *a, int b)
+{
+
+	for (int i = 0; rule[i][0] != 0; i++)
+	{
+
+		if (rule[i][1] == W)
 		{
-			get_time(rule, t, rule[i][0], K, res);
-			sign++;
+			total[*a][b] = rule[i][0];
+			b++;
+
+			get_time(rule, rule[i][0], total, a, b);
 		}
 	}
-	if (sign == 0)
-		
-	return(res);
+	*a += 1;
 }
 
 int read_rule()
 {
-	int K, W;
+	int K, N, W, a = 0;
 	int res = 0;
-	int t[1000];
-	int rule[100000][2];
+	int t[1001]; // 각 건물 설치 시간(N)
+	int rule[100001][2] = {0};
 
-	scanf("%d %d", &t[0], &K);
-	for (int i = 1; i <= t[0]; i++)
+	scanf("%d %d", &N, &K); // N 건물 개수, K 규칙 개수
+	for (int i = 1; i <= N; i++)
 	{
 		scanf("%d", &t[i]);
 	}
-	for (int i = 0; i < K; i++){
-		scanf("%d %d",&rule[i][0], &rule[i][1]);
+	for (int i = 0; i < K; i++)
+	{
+		scanf("%d %d", &rule[i][0], &rule[i][1]);
 	}
-	scanf("%d", &W);
-	res = get_time(rule, t, W, K, 0);
+	scanf("%d", &W); // 필수 설치 건물
 
+	int total[K][1000];
+	get_time(rule, W, total, &a, 0);
+	for (int i = 0; i < K; i++)
+	{
+		for (int j = 0; j < N; j++)
+		{
+			printf("%d ", total[i][j]);
+		}
+		printf("\n");
+	}
+	//	res = maxtime(total);
 	return (res);
 }
 
