8b413e2b16a0a9b959ac305eab5ca7cdafaf100a
Jlnus <zmdk0902@naver.com>
2022-06-01 03:23:25 +0900

    bk2447

--- Silver/C/bk2447.c
+++ Silver/C/bk2447.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
-#include <stdlib.h>
+
+void star(int n, int x, int y)
+{
+	if (x == n)
+	{
+		puts("");
+		star(n, 0, y + 1);
+	}
+	else if (y == n)
+	{
+		return;
+	}
+	else if ((x >= n / 3 && x <= n * 2 / 3 - 1) && (y >= n / 3 && y <= n * 2 / 3 - 1))
+	{
+		printf(" ");
+		x++;
+		star(n, x, y);
+	}
+	else if (x % 3 == 1 && y % 3 == 1)
+	{
+		printf(" ");
+		x++;
+		star(n, x, y);
+	}
+	else
+	{
+		printf("*");
+		x++;
+		star(n, x, y);
+	}
+}
+
 int main()
 {
-	char str[5];
-	gets(str);
+	int n;
+	scanf("%d", &n);
+	star(n, 0, 0);
 }
